ad7bb8a96955176d5bf6c08d0e9ffcadd32bde89
“dam-san” <ryococokuronene@gmail.com>
2021-01-28 02:37:41 +0000

    #

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+  Place.create!(
+   place: '第1工場',
+  )
 
   Admin.create!(
    name: '管理',
